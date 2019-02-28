8076f2389e04935728d2dd0c47c390cee767fda1
Taryn Sauer <tarynsauer@dailykos.com>
2019-02-27 20:47:24 -0600

    Finish todo #2

--- workspace/check_images.py
+++ workspace/check_images.py
@@ -22,13 +22,10 @@
 #    python check_images.py --dir pet_images/ --arch vgg --dogfile dognames.txt
 ##
 
-# Imports python modules
 from time import time, sleep
 
-# Imports print functions that check the lab
 from print_functions_for_lab_checks import *
 
-# Imports functions created for this program
 from get_input_args import get_input_args
 from get_pet_labels import get_pet_labels
 from classify_images import classify_images
@@ -36,23 +33,14 @@ from adjust_results4_isadog import adjust_results4_isadog
 from calculates_results_stats import calculates_results_stats
 from print_results import print_results
 
-# Main program function defined below
 def main():
     start_time = time()
 
     in_arg = get_input_args()
 
-    # Function that checks command line arguments using in_arg
     check_command_line_arguments(in_arg)
 
-    # TODO 2: Define get_pet_labels function within the file get_pet_labels.py
-    # Once the get_pet_labels function has been defined replace 'None'
-    # in the function call with in_arg.dir  Once you have done the replacements
-    # your function call should look like this:
-    #             get_pet_labels(in_arg.dir)
-    # This function creates the results dictionary that contains the results,
-    # this dictionary is returned from the function call as the variable results
-    results = get_pet_labels(None)
+    results = get_pet_labels(in_arg.dir)
 
     # Function that checks Pet Images in the results Dictionary using results
     check_creating_pet_image_labels(results)
