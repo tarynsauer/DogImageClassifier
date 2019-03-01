376920f5bbc5b9a9766cef9b90e521515c4b45b8
Taryn Sauer <tarynsauer@dailykos.com>
2019-03-01 16:40:31 -0600

    Complete TODO #3/Classifier.py

--- workspace/check_images.py
+++ workspace/check_images.py
@@ -45,16 +45,7 @@ def main():
     # Function that checks Pet Images in the results Dictionary using results
     check_creating_pet_image_labels(results)
 
-
-    # TODO 3: Define classify_images function within the file classiy_images.py
-    # Once the classify_images function has been defined replace first 'None'
-    # in the function call with in_arg.dir and replace the last 'None' in the
-    # function call with in_arg.arch  Once you have done the replacements your
-    # function call should look like this:
-    #             classify_images(in_arg.dir, results, in_arg.arch)
-    # Creates Classifier Labels with classifier function, Compares Labels,
-    # and adds these results to the results dictionary - results
-    classify_images(None, results, None)
+    classify_images(in_arg.dir, results, in_arg.arch)
 
     # Function that checks Results Dictionary using results
     check_classifying_images(results)
