641e17b1eb786d9e218a40e2d8fec36609e742a6
Taryn Sauer <tarynsauer@dailykos.com>
2019-03-01 22:38:10 -0600

    TODO #5: Calculating Restuls

--- workspace/check_images.py
+++ workspace/check_images.py
@@ -55,13 +55,6 @@ def main():
     # Function that checks Results Dictionary for is-a-dog adjustment using results
     check_classifying_labels_as_dogs(results)
 
-
-    # TODO 5: Define calculates_results_stats function within the file calculates_results_stats.py
-    # This function creates the results statistics dictionary that contains a
-    # summary of the results statistics (this includes counts & percentages). This
-    # dictionary is returned from the function call as the variable results_stats
-    # Calculates results of run and puts statistics in the Results Statistics
-    # Dictionary - called results_stats
     results_stats = calculates_results_stats(results)
 
     # Function that checks Results Statistics Dictionary using results_stats
